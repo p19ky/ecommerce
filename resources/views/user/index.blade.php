29223bb9ad90f7393ea171106a1d694cfbc8d017
p19ky <61684270+p19ky@users.noreply.github.com>
2020-08-18 22:19:06 +0300

    commit some minor changes

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,13 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Greetings') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     {{ __('You are logged in!') }} {{ Auth::user()->name }}
